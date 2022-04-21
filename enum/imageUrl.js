bd82b67d7928ab8f86c48cc583c553ad2786ea11
Peng Junzhi <201250214@smail.nju.edu.cn>
2022-04-22 00:40:51 +0800

    feat: image url enum

---
+++ enum/imageUrl.js
@@ -0,0 +1,46 @@
+/**
+ * 静态资源的url
+ * 变量名与文件夹中的图片命名相同
+ * 使用：在需要引入的js文件里import * as IMG from '本文件路径'
+ * 随后：在需要的地方以 "IMG.xxx" 使用即可
+ */
+
+// 核酸 - 首页
+const ICON_CALENDER = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/index_hesuan/icon_calender.png'
+const VECTOR_PIC = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/index_hesuan/vector_pic.png'
+// 核酸 - 列表
+const REMIND = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/lists/remind.png'
+const STATE_MISS = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/lists/state_miss.png'
+const STATE_ONGOING = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/lists/state_ongoing.png'
+const STATE_OVER = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/lists/state_over.png'
+// NOTICE和RETURN是核酸 - 列表与核酸 - 我的公用
+const NOTICE = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/lists/notice.png'
+const RETURN = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/lists/return.png'
+// 导航栏
+const HESUAN = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/nav/hesuan.png'
+const HESUAN_SELECTED = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/nav/hesuan_isSelected.png'
+const IHELP = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/nav/ihelp.png'
+const IHELP_SELECTED = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/nav/ihelp_isSelected.png'
+const MSG = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/nav/msg.png'
+const MSG_SELECTED = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/nav/msg_isSelected.png'
+const MY = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/nav/my.png'
+const MY_SELECTED = 'https://peng-img.oss-cn-shanghai.aliyuncs.com/wechat/nav/my.png'
+
+export {
+  ICON_CALENDER,
+  VECTOR_PIC,
+  REMIND,
+  STATE_MISS,
+  STATE_ONGOING,
+  STATE_OVER,
+  NOTICE,
+  RETURN,
+  HESUAN,
+  HESUAN_SELECTED,
+  IHELP,
+  IHELP_SELECTED,
+  MSG,
+  MSG_SELECTED,
+  MY,
+  MY_SELECTED,
+}
