47229405fc4cedd0cdf55f3ca08f8292cde59c11
201250154 <Cyr607716>
2022-04-23 16:26:48 +0800

    fix：indexNA js

--- nucleic-acid/pages/index_NA/index_NA.js
+++ nucleic-acid/pages/index_NA/index_NA.js
@@ -1,12 +1,23 @@
 // nucleic-acid/pages/index_NA/index_NA.js
 import * as IMG from '../../../enum/imageUrl'
 Page({
-  data :{
-    img_url: {
-      ICON_CALENDER: IMG.ICON_CALENDER,
-      VECTOR_PIC: IMG.VECTOR_PIC
-    }
-  },
+    data :{
+        num_appointment:0,
+        num_detection:0,
+        num_report:0,
+        ICON_CALENDER: IMG.ICON_CALENDER,
+        VECTOR_PIC: IMG.VECTOR_PIC
+    },
+
+    onLoad: function(options) {
+        let that = this;
+        that.setData({
+            num_appointment:1,
+            num_detection:2,
+            num_report:8,//还不会调接口的写法
+        })
+    },
+
     gotoAppointment: function (options) {
         wx.navigateTo({
               url: '../appointment/appointment',
