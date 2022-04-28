bbae9dcc52b4fb3a691eaa8a372ed3b1b494dc54
201250154 <Cyr607716>
2022-04-28 23:10:20 +0800

    feat：我的帮忙 展示tips

--- my/pages/my_help/my_help.js
+++ my/pages/my_help/my_help.js
@@ -1,11 +1,30 @@
 // my/pages/my_help/my_help.js
+import * as IMG from '../../../enum/imageUrl'
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
+        QUESTION:IMG.ICON_QUESTION,
+        tips:[
+            {'icon':IMG.ICON_OVER_BLACK,'tip':'正在进行的帖子，右滑可变为已解决状态。'},
+            {'icon':IMG.ICON_DELETE_BLACK,'tip':'已解决和未解决的帖子，可以左滑进行删除。系统默认显示一个月内的所有帖子。'},
+            {'icon':IMG.ICON_REDO_BLACK,'tip':'到期未解决的帖子，左滑跳转到发布页面，修改日期即可重新发布。'},
+        
+          ],
+    },
 
+    showTips: function () {
+        this.setData({
+            isTipsTrue: true
+        })
+    },
+
+    hideTips: function () {
+        this.setData({
+            isTipsTrue: false
+        })
     },
 
     /**
