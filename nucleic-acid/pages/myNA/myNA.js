82732c40f14438270d7a8d74710de4d133a98c95
Peng Junzhi <201250214@smail.nju.edu.cn>
2022-04-28 16:19:04 +0800

    style：缩进

--- nucleic-acid/pages/myNA/myNA.js
+++ nucleic-acid/pages/myNA/myNA.js
@@ -11,7 +11,6 @@ Page({
     img_url: {
       notice: IMG.NOTICE,
     },
-
     successDate :[],//“已完成”的核酸日期
     failDate:[],//“未完成”的核酸日期
     successList:[],//“已完成”的核酸列表
@@ -48,7 +47,7 @@ Page({
         'Authorization': wx.getStorageSync('token')
       },
       method: "GET",
-      success: (res)=> {
+      success: (res) => {
         const resSet = res.data.data
         let list1 = []
         let list2 = []
@@ -121,38 +120,4 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
