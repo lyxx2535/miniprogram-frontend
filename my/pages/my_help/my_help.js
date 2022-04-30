16acf3bacef9dd8c28920d903477ab259c6fe839
84368 <843683531@qq.com>
2022-04-30 21:17:03 +0800

    feat：添加滑动条粗略样式

--- my/pages/my_help/my_help.js
+++ my/pages/my_help/my_help.js
@@ -18,7 +18,10 @@ Page({
         test_img: IMG.VECTOR_PIC,
         img_url: {
             solved: IMG.STATE_SOLVED,
-            unsolved: IMG.STATE_UNSOLVED
+            unsolved: IMG.STATE_UNSOLVED,
+            delete: IMG.ICON_DELETE_BLACK,
+            over: IMG.ICON_OVER_BLACK,
+            redo: IMG.ICON_REDO_BLACK,
           },
     },
 
@@ -61,6 +64,18 @@ Page({
             showmenu: true,
         })
     },
+    // 删除该条信息
+    deleteMsg(e){
+        console.log("delete")
+    },
+    // 编辑该条信息
+    editMsg(e){
+        console.log("edit")
+    },
+    // 结束该条信息
+    overMsg(e){
+        console.log("over")
+    },
     //获得“帮忙”信息，让“进行中”数据展示在“已截止”之前
     async getRenderHelpList(){
         const res = await api._rh_list_byUser();
@@ -81,6 +96,7 @@ Page({
             renderHelpList: ongoingList.concat(endList)
         })
     },
+
     /**
      * 生命周期函数--监听页面加载
      */
--- my/pages/my_seekHelp/my_seekHelp.js
+++ my/pages/my_seekHelp/my_seekHelp.js
@@ -18,7 +18,10 @@ Page({
         test_img: IMG.VECTOR_PIC,
         img_url: {
             solved: IMG.STATE_SOLVED,
-            unsolved: IMG.STATE_UNSOLVED
+            unsolved: IMG.STATE_UNSOLVED,
+            delete: IMG.ICON_DELETE_BLACK,
+            over: IMG.ICON_OVER_BLACK,
+            redo: IMG.ICON_REDO_BLACK,
           },
     },
 
@@ -60,6 +63,18 @@ Page({
             current: url,
             showmenu: true,
         })
+    },
+       // 删除该条信息
+       deleteMsg(e){
+        console.log("delete")
+    },
+    // 编辑该条信息
+    editMsg(e){
+        console.log("edit")
+    },
+    // 结束该条信息
+    overMsg(e){
+        console.log("over")
     },
     // 获得“帮忙”信息，让“进行中”数据展示在“已截止”之前
     async getSeekHelpList(){
